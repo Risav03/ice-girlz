34beb8e228b1f140eb9c77bd3a7538b4eaa840c5
Risav03 <needleweed03@gmail.com>
2025-04-28 20:46:35 +0530

    y padding low

--- components/raffles/raffleHolder.tsx
+++ components/raffles/raffleHolder.tsx
@@ -11,7 +11,7 @@ export const RaffleHolder = ({ raffles, loading }: { raffles: any, loading:boole
     if(raffles.length > 0)
     return (
         // <div className="min-h-screen h-screen w-screen md:px-10 max-md:px-4 pt-24 pb-12 flex items-center justify-center">
-            <div className='md:w-[70vw] max-md: mt-2 md:min-w-[700px] bg-white max-md:w-full md:pb-4 pb-1 md:pt-8 pt-4 rounded-2xl border-[1px] items-center border-icePurp '>
+            <div className='md:w-[70vw] max-md: mt-2 md:min-w-[700px] bg-white max-md:w-full pt-4 rounded-2xl border-[1px] items-center border-icePurp '>
                 <div className='w-full relative'>
                     <div className='flex items-center w-full md:gap-3 gap-1'>
                         {page > 0 ?  <button onClick={()=>{if(page > 0){setPage(prev => prev-1)}}} className='md:w-[3%] max-md:w-[6%] max-sm:w-[10%] border-y-[1px] border-r-[1px] border-icePurp active:scale-110 rounded-r-xl py-4 hover:bg-icePurp group duration-200'>
